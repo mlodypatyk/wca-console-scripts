16fb5289ed266b33857c8e67e120c448c0b79147
Przemysław Rogalski <przemyslaw.rogalski121@gmail.com>
2026-02-05 15:44:41 +0100

    Fix labels

--- staffing-rankings.js
+++ staffing-rankings.js
@@ -2,7 +2,7 @@ fetch('https://www.worldcubeassociation.org/api/v0/competitions/GLSBigCubesGdans
   const competitorsWithRank = wcif.persons.map(person => 
     [person, person.assignments.filter(a => ['staff-scrambler','staff-runner','staff-judge'].includes(a.assignmentCode)).length]
   );
-  console.log('| Staffing | Person |')
+  console.log('| Staffing | Person | WCA ID | Roles |')
   const ranking = competitorsWithRank
     .sort(([p1, r1], [p2, r2]) => r2 - r1)
     .map(([person, rank]) => `| ${rank} | ${person.name} | ${person.wcaId} | ${person.roles.join(',')}`)
